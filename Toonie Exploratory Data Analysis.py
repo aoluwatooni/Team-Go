d966d4ed424a49d29aa216243d82924cfc0211f0
aoluwatooni <53844337+aoluwatooni@users.noreply.github.com>
2019-09-27 14:37:28 +0100

    Add files via upload

---
+++ Toonie Exploratory Data Analysis.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+df = pd.read_csv (r'C:\Users\aoluw\Desktop\Github\Team-Go\Lucid blog.csv\contact_settings.csv')
+print (df)
+
+
+# In[2]:
+
+
+df = pd.read_csv (r'C:\Users\aoluw\Desktop\Github\Team-Go\Lucid blog.csv\maillists.csv')
+print (df)
+
+
+# In[3]:
+
+
+df = pd.read_csv (r'C:\Users\aoluw\Desktop\Github\Team-Go\Lucid blog.csv\notifications.csv')
+print (df)
+
+
+# In[4]:
+
+
+df = pd.read_csv (r'C:\Users\aoluw\Desktop\Github\Team-Go\Lucid blog.csv\posts.csv')
+print (df)
+
+
+# In[5]:
+
+
+df = pd.read_csv (r'C:\Users\aoluw\Desktop\Github\Team-Go\Lucid blog.csv\users.csv')
+print (df)
+
+
+# In[ ]:
+
+
+
+
